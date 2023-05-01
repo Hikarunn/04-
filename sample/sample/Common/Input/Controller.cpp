24aed1b142417aaaeb38bb24570734529e0a69f2
Hikarunn <hikaru.0312.0708@gmail.com>
2023-05-01 13:02:58 +0900

    コントローラークラスの内部作成

--- sample/sample/Common/Input/Controller.cpp
+++ sample/sample/Common/Input/Controller.cpp
@@ -1,1 +1,137 @@
+#include <DxLib.h>
+#include <time.h>
 #include "Controller.h"
+
+#include "../Debug.h"
+
+Controller::Controller()
+{
+    cursorSpeed_ = 120.0f;
+    cursorPos_ = lpSceneManager.screenSize_<float> / 2.0f;
+    isPress_ = false;
+    isFinish_ = false;
+    PressCount_ = 0;
+    data_.fill(std::make_pair(false, false));
+}
+
+Controller::~Controller()
+{
+}
+
+CountType Controller::GetCountType(void)
+{
+   
+}
+
+bool Controller::MousePress(InputID id)
+{
+    if (InputID::Attack == id)
+    {
+        if (GetMouseInput() & MOUSE_INPUT_LEFT)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Controller::Press(InputID id)
+{
+    return  data_[static_cast<size_t>(id)].first;
+}
+
+bool Controller::Pressed(InputID id)
+{
+    return data_[static_cast<size_t>(id)].first && !data_[static_cast<size_t>(id)].second;
+}
+
+bool Controller::Released(InputID id)
+{
+    return !data_[static_cast<size_t>(id)].first && data_[static_cast<size_t>(id)].second;
+}
+
+bool Controller::NotPress(InputID id)
+{
+    return !data_[static_cast<size_t>(id)].first;
+}
+
+bool Controller::IsNeutral()
+{
+    bool rtnflag = false;
+    for (int n = 0; n < static_cast<int>(InputID::Max); n++)
+    {
+        rtnflag |= data_[static_cast<size_t>(static_cast<InputID>(n))].first;
+    }
+    return !rtnflag;
+}
+
+bool Controller::IsAnyPress()
+{
+    bool rtnflag = false;
+    for (int n = 0; n < static_cast<int>(InputID::Max); n++)
+    {
+        rtnflag |= data_[static_cast<size_t>(static_cast<InputID>(n))].first && !data_[static_cast<size_t>(static_cast<InputID>(n))].second;
+    }
+    return !rtnflag;
+}
+
+
+std::string Controller::LongPress(InputID id, double limit, double delta)
+{
+    if (!isPress_)
+    {
+        if (Press(InputID::Attack))
+        {
+            isPress_ = true;
+            PressCount_ = 0;
+        }
+    }
+    else
+    {
+        PressCount_ += (float)delta;
+        if (!Press(InputID::Attack))
+        {
+            isPress_ = false;
+            isFinish_ = true;
+        }
+
+        if (isFinish_)
+        {
+            isFinish_ = false;
+            if (PressCount_ < limit)
+            {
+                return "Attack";
+            }
+            else
+            {
+                return "LongAttack";
+            }
+        }
+    }
+    return "";
+}
+
+const InputData& Controller::Get(void)
+{
+    return data_;
+}
+
+bool Controller::PressDecision(void) const
+{
+    return  decisionData_.first;
+}
+
+bool Controller::PressedDecision(void) const
+{
+    return decisionData_.first && !decisionData_.second;
+}
+
+bool Controller::PressCancel(void) const
+{
+    return cancelData_.first;
+}
+
+bool Controller::PressdCancel(void) const
+{
+    return cancelData_.first && !cancelData_.second;
+}
--- sample/sample/Common/Input/InputID.h
+++ sample/sample/Common/Input/InputID.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <type_traits>
+
+// ƒL[î•ñ
+enum class InputID
+{
+	Jump,
+	Attack,
+	MainTriger1,
+	MainTriger2,
+	MainTriger3,
+	MainTriger4,
+	SubTriger1,
+	SubTriger2,
+	SubTriger3,
+	SubTriger4,
+	LockOn,
+	CameraFlont,
+	Max
+};
+
+static InputID begin(InputID) { return InputID::Jump; }
+static InputID end(InputID) { return InputID::Max; }
+static InputID operator++(InputID& state) { return (state = static_cast<InputID>(std::underlying_type<InputID>::type(state) + 1)); }
+static InputID operator*(const InputID& state) { return state; }
